1b39464e41778a24e42a217d1160a746d8577b2b
AtharvGaur <atharava112@gmail.com>
2025-05-07 13:31:59 +0530

    Refactored Sign in and sign up pages

--- app/(app)/sign-in/[[...sign-in]]/layout.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function layout({ children }) {
-  return (
-    <div className='flex w-full justify-center min-h-screen items-center'>{children}</div>
-  )
-}
-
-export default layout
